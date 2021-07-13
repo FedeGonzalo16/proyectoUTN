21477e9871de3f91151c96947d530da0c40f86b7
Federico Prieto <82186644+FedeGonzalo16@users.noreply.github.com>
2021-07-12 22:07:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,4 @@ const signinupRouter = require('./routes/sign-in-up');
 const app = express();
 app.use(express.json());
 
-/* router.use('./views/contacto', contactoRouter);
-router.use('./views/sign-in-up', signinupRouter); */
-
 module.exports = app;
